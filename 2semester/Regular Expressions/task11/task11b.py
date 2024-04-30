16083e65a0c7591a526ceb5fe082fa7c0e743ffb
eeelyl <mixasar224@gmail.com>
2024-04-30 11:52:36 +0400

    Исправил регулярные выражения.

--- 2semester/Regular Expressions/task11/task11b.py
+++ 2semester/Regular Expressions/task11/task11b.py
@@ -1,12 +1,13 @@
-def snake_to_camel_case(text):
-    # Разбиваем строку по подчеркиваниям, преобразуем первую букву каждого слова в заглавную
-    words = [word.capitalize() for word in text.split('_')]
-    # Объединяем слова и убираем пробелы
-    camel_case_text = ''.join(words)
-    return camel_case_text
+import re
 
+def snake_to_camel_case(string):
+    # Используем регулярное выражение для поиска слов, разделенных символом подчеркивания
+    words = re.split(r'_', string)
+    # Каждое слово кроме первого, преобразуем первую букву в заглавную
+    camel_case = ''.join([words[0]] + [word.capitalize() for word in words[1:]])
+    return camel_case
 
-# Пример использования:
-text = "btw... -what- * -do * -you-call-that-naming-style? -snake-case?"
-camel_case_text = snake_to_camel_case(text)
-print("Результат преобразования в camel-case:", camel_case_text)
+# Пример использования
+snake_string = "btw_what_do_you_call_that_naming_style"
+camel_string = snake_to_camel_case(snake_string)
+print(camel_string)
