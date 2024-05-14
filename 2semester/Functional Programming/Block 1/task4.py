16b6c7f29e47bf30fd227b91c3b6190211ba3b3f
eeelyl <mixasar224@gmail.com>
2024-05-14 12:53:10 +0400

    Переделать 4 в функциональном программировании и добавить проверку времени в 8.

--- 2semester/Functional Programming/Block 1/task4.py
+++ 2semester/Functional Programming/Block 1/task4.py
@@ -1,24 +1,16 @@
 def sort1(s):
-    res = ''
-    pos = list(filter(lambda x: int(x) >= 0, s.split(' ')))[::-1]
-    neg = list(filter(lambda x: int(x) < 0, s.split(' ')))[::-1]
-    for i in range(len(pos)):
-        res += pos.pop() + ' '
-        if neg:
-            res += neg.pop() + ' '
-    res += ' '.join(neg)
+    res = [int(num) for num in s.split()]
+    res.sort(key=lambda x: (x < 0, x))
     return res
 
 
 def sort2(s):
-    numbers = sorted(list(map(int, s.split(' '))))
-    pos = [str(x) for x in numbers if x >= 0]
-    neg = [str(x) for x in numbers if x < 0]
-    res = ' '.join(pos) + ' ' + ' '.join(neg)
+    res = [int(num) for num in s.split()]
+    res.sort()
     return res
 
 
-s = '6 -1 3 -5 -15 4 1 9 8 6 -3 -4 12 -10 7'
+inp = '6 -1 3 -5 -15 4 1 9 8 6 -3 -4 12 -10 7'
 
-print(sort1(s))
-print(sort2(s))
+print(sort1(inp))
+print(sort2(inp))
